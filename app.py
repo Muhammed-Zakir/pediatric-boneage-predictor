79b909c3beb8fcc10426bbace7785c0d6c2a249f
Zakir <138rmz@gmail.com>
2025-08-11 14:46:50 +0530

    Updated URLs

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import streamlit as st
 st.set_page_config(page_title="Pediatric Age Predictor", layout="wide")
 
 # Explicitly define pages with custom titles
-home_page = st.Page("pages/0_home.py", title="Home")
-data_info = st.Page("pages/1_Data_Info.py", title="Data Information")
-data_sample = st.Page("pages/2_Data_Sampling.py", title="Data Sampling")
-ml_page = st.Page("pages/3_Machine_Learning.py", title="Machine Learning")
-predict_page = st.Page("pages/4_Predict_Bone_Age.py", title="Predict Bone Age")
+home_page = st.Page("pages/Home.py", title="Home")
+data_info = st.Page("pages/Data_Info.py", title="Data Information")
+data_sample = st.Page("pages/Data_Sampling.py", title="Data Sampling")
+ml_page = st.Page("pages/Machine_Learning.py", title="Machine Learning")
+predict_page = st.Page("pages/Predict_Bone_Age.py", title="Predict Bone Age Test")
 
 # Create navigation
 nav = st.navigation([home_page, data_info, data_sample, ml_page, predict_page])
